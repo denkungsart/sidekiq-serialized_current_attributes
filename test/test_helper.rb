b12d682c796431cf34ff9507cc4dd1295c5da4f2
Fabian Schwahn <fabian.schwahn@gmail.com>
2024-10-28 11:56:11 +0100

    Fix order of requires in test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "sidekiq/serialized_current_attributes"
 require "sidekiq/testing"
 require "active_support/all"
 require "active_job"
+require "sidekiq/serialized_current_attributes"
 
 require "minitest/autorun"
